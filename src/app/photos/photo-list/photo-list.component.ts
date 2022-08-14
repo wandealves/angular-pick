fb802d6e941e5d574ecfb567fbb32edd3ecfb2b7
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-08-14 15:58:44 -0300

    feat: criação do resolve de rotas

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { Photo } from "../photo/photo";
-import { PhotoService } from "../photo/photo.service";
 
 @Component({
   selector: "ap-photo-list",
@@ -14,15 +13,11 @@ export class PhotoListComponent implements OnInit {
   filter: string = "";
 
   constructor(
-    private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params["userName"];
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => (this.photos = photos));
+    this.photos = this.activatedRoute.snapshot.data["photos"];
   }
 
   onKeyup(event: Event) {
