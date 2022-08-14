9ed228e390e720e73bd9da48828752ea09777a1f
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-08-14 14:09:31 -0300

    feat: criação do serviço para consumir a API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { PhotosModule } from "./photos/photos.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PhotosModule],
+  imports: [BrowserModule, HttpClientModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent]
 })
