8f15c07f62fcee6273b0ed1718cabe95abd6ba83
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-08-14 16:09:20 -0300

    feat: adição debounce

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
 
 import { Photo } from "../photo/photo";
 
@@ -8,19 +10,25 @@ import { Photo } from "../photo/photo";
   templateUrl: "./photo-list.component.html",
   styleUrls: ["./photo-list.component.css"]
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = "";
+  debounce: Subject<string> = new Subject<string>();
 
-  constructor(
-    private activatedRoute: ActivatedRoute
-  ) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data["photos"];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => (this.filter = filter));
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
   onKeyup(event: Event) {
-    this.filter = (event.target as HTMLInputElement).value;
+    this.debounce.next((event.target as HTMLInputElement).value);
   }
 }
