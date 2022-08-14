0f6aae43eb761e06b6199fd1e5849d35f1897334
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-08-14 12:40:43 -0300

    feat: carregar varia sfotos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-pick';
+  photos = [
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/400px-Sultan_the_Barbary_Lion.jpg",
+      description: "Leão"
+    },
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg",
+      description: "Leoa"
+    }
+  ];
 }
