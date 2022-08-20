c4dbd4796e2063d407f8a4cf5de7dd9379303a0f
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-08-20 14:46:32 -0300

    feat: criação diretivas

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -8,6 +8,7 @@ import { PhotosComponent } from "./photos/photos.component";
 import { PhotoListComponent } from "./photo-list.component";
 import { CardModule } from "./../../shared/components/card/card.module";
 import { SearchComponent } from "./search/search.component";
+import { DarkenOnHoverModule } from "./../../shared/directives/darken-on-hover/darken-on-hover.module";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,6 @@ import { SearchComponent } from "./search/search.component";
     FilterByDescriptionpipe,
     SearchComponent
   ],
-  imports: [CommonModule, PhotoModule, CardModule]
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule]
 })
 export class PhotoListModule {}
