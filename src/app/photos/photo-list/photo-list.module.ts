2fb797be8739a60b7d7663da734f72fc7e8738de
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-08-20 14:26:13 -0300

    feat: separação do campo de pesquisa

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,13 +7,15 @@ import { LoadButtonComponent } from "./load-button/load-button.component";
 import { PhotosComponent } from "./photos/photos.component";
 import { PhotoListComponent } from "./photo-list.component";
 import { CardModule } from "./../../shared/components/card/card.module";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
   declarations: [
     PhotoListComponent,
     PhotosComponent,
     LoadButtonComponent,
-    FilterByDescriptionpipe
+    FilterByDescriptionpipe,
+    SearchComponent
   ],
   imports: [CommonModule, PhotoModule, CardModule]
 })
