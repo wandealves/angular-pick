33e6a8e87ff2591d4dff1cf9ed16a8752500aa69
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-08-20 13:03:20 -0300

    feat: criação botão de carregar mais fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from "@angular/router";
 import { Subject } from "rxjs";
 import { debounceTime } from "rxjs/operators";
 
+import { PhotoService } from "./../photo/photo.service";
 import { Photo } from "../photo/photo";
 
 @Component({
@@ -14,10 +15,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = "";
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = "";
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params["userName"];
     this.photos = this.activatedRoute.snapshot.data["photos"];
     this.debounce
       .pipe(debounceTime(300))
@@ -30,5 +38,18 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   onKeyup(event: Event) {
     this.debounce.next((event.target as HTMLInputElement).value);
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = [...this.photos, ...photos];
+        //this.photos = this.photos.concat(photos);
+
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
   }
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -7,9 +7,10 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescriptionpipe } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionpipe],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionpipe, LoadButtonComponent],
   imports: [CommonModule, HttpClientModule]
 })
 export class PhotosModule {}
